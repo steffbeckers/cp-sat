236cd4ca5d96aa0187b38ad218ce8d0ae3a4bc98
Steff Beckers <steff@steffbeckers.eu>
2023-10-05 20:05:34 +0200

    Evil sudoku test added

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -44,19 +44,34 @@ for (int i = 0; i < sqrtSize; i++)
 }
 
 // Define the initial Sudoku puzzle configuration
-// https://en.wikipedia.org/wiki/Sudoku
+// Sudoku 1: https://en.wikipedia.org/wiki/Sudoku
+// int[][] initialGrid = new int[][] {
+//     new int[] { 5, 3, 0,  0, 7, 0,  0, 0, 0 },
+//     new int[] { 6, 0, 0,  1, 9, 5,  0, 0, 0 },
+//     new int[] { 0, 9, 8,  0, 0, 0,  0, 6, 0 },
+
+//     new int[] { 8, 0, 0,  0, 6, 0,  0, 0, 3 },
+//     new int[] { 4, 0, 0,  8, 0, 3,  0, 0, 1 },
+//     new int[] { 7, 0, 0,  0, 2, 0,  0, 0, 6 },
+
+//     new int[] { 0, 6, 0,  0, 0, 0,  2, 8, 0 },
+//     new int[] { 0, 0, 0,  4, 1, 9,  0, 0, 5 },
+//     new int[] { 0, 0, 0,  0, 8, 0,  0, 7, 9 }
+// };
+
+// Sudoku 2: https://sudoku.com/evil/
 int[][] initialGrid = new int[][] {
-    new int[] { 5, 3, 0,  0, 7, 0,  0, 0, 0 },
-    new int[] { 6, 0, 0,  1, 9, 5,  0, 0, 0 },
-    new int[] { 0, 9, 8,  0, 0, 0,  0, 6, 0 },
+    new int[] { 9, 0, 0,  0, 1, 0,  0, 0, 6 },
+    new int[] { 2, 0, 0,  5, 0, 6,  3, 0, 0 },
+    new int[] { 0, 5, 0,  0, 4, 0,  0, 0, 0 },
 
-    new int[] { 8, 0, 0,  0, 6, 0,  0, 0, 3 },
-    new int[] { 4, 0, 0,  8, 0, 3,  0, 0, 1 },
-    new int[] { 7, 0, 0,  0, 2, 0,  0, 0, 6 },
+    new int[] { 0, 2, 0,  7, 0, 9,  0, 0, 4 },
+    new int[] { 3, 0, 0,  0, 0, 0,  0, 8, 0 },
+    new int[] { 0, 0, 0,  0, 5, 0,  0, 0, 0 },
 
-    new int[] { 0, 6, 0,  0, 0, 0,  2, 8, 0 },
-    new int[] { 0, 0, 0,  4, 1, 9,  0, 0, 5 },
-    new int[] { 0, 0, 0,  0, 8, 0,  0, 7, 9 }
+    new int[] { 0, 0, 0,  0, 0, 1,  0, 0, 0 },
+    new int[] { 0, 7, 0,  6, 0, 2,  0, 0, 9 },
+    new int[] { 0, 0, 9,  0, 0, 0,  4, 0, 0 }
 };
 
 // Add constraints to enforce the initial configuration
@@ -82,7 +97,7 @@ CpSolverStatus status = solver.Solve(model);
 // Print the solution
 if (status == CpSolverStatus.Feasible || status == CpSolverStatus.Optimal)
 {
-    Console.WriteLine("Solution found:");
+    Console.WriteLine($"{status} solution found:");
 
     for (int i = 0; i < size; i++)
     {
@@ -94,14 +109,32 @@ else
     Console.WriteLine("No solution found.");
 }
 
+// Sudoku 1
 // Output:
-// Solution found:
-// 5 3 4 6 7 8 9 1 2
-// 6 7 2 1 9 5 3 4 8
-// 1 9 8 3 4 2 5 6 7
-// 8 5 9 7 6 1 4 2 3
-// 4 2 6 8 5 3 7 9 1
-// 7 1 3 9 2 4 8 5 6
-// 9 6 1 5 3 7 2 8 4
-// 2 8 7 4 1 9 6 3 5
-// 3 4 5 2 8 6 1 7 9
+// Optimal solution found:
+// 5 3 4  6 7 8  9 1 2
+// 6 7 2  1 9 5  3 4 8
+// 1 9 8  3 4 2  5 6 7
+
+// 8 5 9  7 6 1  4 2 3
+// 4 2 6  8 5 3  7 9 1
+// 7 1 3  9 2 4  8 5 6
+
+// 9 6 1  5 3 7  2 8 4
+// 2 8 7  4 1 9  6 3 5
+// 3 4 5  2 8 6  1 7 9
+
+// Sudoku 2
+// Output:
+// Optimal solution found:
+// 9 8 3  2 1 7  5 4 6
+// 2 4 7  5 8 6  3 9 1
+// 6 5 1  9 4 3  2 7 8
+
+// 5 2 8  7 6 9  1 3 4
+// 3 9 6  1 2 4  7 8 5
+// 7 1 4  3 5 8  9 6 2
+
+// 8 3 2  4 9 1  6 5 7
+// 4 7 5  6 3 2  8 1 9
+// 1 6 9  8 7 5  4 2 3
