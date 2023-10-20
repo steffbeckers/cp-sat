473bda673f9307933b6dd83a85a3a78a1291038c
Steff Beckers <steff@steffbeckers.eu>
2023-10-20 20:14:41 +0200

    Sandbox project

---
+++ Sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿using Google.OrTools.Sat;
+
+CpModel model = new CpModel();
+
+int numberCount = 10;
+
+IntVar x = model.NewIntVar(0, numberCount, "x");
+IntVar y = model.NewIntVar(0, numberCount, "y");
+
+model.AddAllDifferent(new IntVar[] { x, y });
+model.Add(x == y * 2);
+model.Add(x > 5);
+
+CpSolver solver = new CpSolver();
+
+CpSolverStatus status = solver.Solve(model);
+
+// Print the solution
+if (status == CpSolverStatus.Feasible || status == CpSolverStatus.Optimal)
+{
+	Console.WriteLine($"{status} solution found:");
+
+	Console.WriteLine($"x: {solver.Value(x)}");
+	Console.WriteLine($"y: {solver.Value(y)}");
+}
+else
+{
+	Console.WriteLine("No solution found.");
+}
